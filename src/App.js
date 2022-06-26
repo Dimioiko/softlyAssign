590659ad7a8070017d03a049e570106949f5f806
dimitris oikonomou <doikonomou@prudea.com>
2022-06-27 00:50:48 +0300

    Add App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppHeader from "./components/AppHeader";
+import AppBody from "./components/AppBody";
+import ExpandMenu from "./components/ExpandMenu";
+import ExpandCategories from "./components/ExpandCategories";
+import AppFooter from "./components/AppFooter";
+import styled from "styled-components";
+import ChatImage from "./assets/chat.svg";
+import { useState } from "react";
 
 function App() {
+  const [sidebarStatus, setSidebarStatus] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <AppHeader
+        sidebarStatus={sidebarStatus}
+        showSideBar={() => setSidebarStatus("ExpandMenu")}
+        hideSideBar={() => setSidebarStatus(undefined)}
+      />
+      {sidebarStatus === "ExpandMenu" ? (
+        <ExpandMenu showSideBar={() => setSidebarStatus("ExpandCategories")} />
+      ) : sidebarStatus === "ExpandCategories" ? (
+        <ExpandCategories
+          goPreviousSideBar={() => setSidebarStatus("ExpandMenu")}
+        />
+      ) : (
+        <>
+          <AppBody />
+          <AppFooter />
+        </>
+      )}
+      <AppChatContainer>
+        <AppChatImage src={ChatImage} />
+      </AppChatContainer>
     </div>
   );
 }
 
 export default App;
+
+const AppChatContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0px;
+  z-index: 100;
+`;
+const AppChatImage = styled.img``;
